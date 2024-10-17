64bfad8b24a7c1363293667bfec669dddf371155
Ahmed Abbas <111700915+Ahmed-Abbas-2077@users.noreply.github.com>
2024-10-17 04:00:18 +0300

    Implement GET /todos endpoint to retrieve all todos

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+todos = ['eat', 'sleep', 'code']
+
 
 @app.route('/')
-def home():
+def index():
     return "Todo List API is running!"
+
+
+@app.route('/todos', methods=['GET'])
+def getTodos():
+    return jsonify(todos), 200
 
 
 if __name__ == '__main__':
